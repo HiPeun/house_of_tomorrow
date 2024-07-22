701ae75e9ca1987e09618548153eff97e71fa04b
charles <rmswo417@naver.com>
2024-07-22 23:45:51 +0900

    검색창 생성 후 입력창 삭제버튼 생성

--- lib/src/view/shopping/shopping_view.dart
+++ lib/src/view/shopping/shopping_view.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:house_of_tomorrow/src/service/theme_service.dart';
 import 'package:house_of_tomorrow/theme/component/bottom_sheet/base_bottom_sheet.dart';
 import 'package:house_of_tomorrow/theme/component/bottom_sheet/setting_bottom_sheet.dart';
 import 'package:house_of_tomorrow/theme/component/button/button.dart';
+import 'package:house_of_tomorrow/theme/component/input_field.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
@@ -12,34 +14,52 @@ class ShoppingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(S.current.shopping),
-        actions: [
-          ///설정버튼
-          Button(
-            icon: 'option',
-            type: ButtonType.flat,
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-
-                builder: (context) {
-                  return const SettingBottomSheet();
-                },
-              );
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: context.read<ThemeService>().toggleTheme,
-          child: Text(
-            "Toggle Theme",
-            style: context.typo.headline6,
-          ),
+        appBar: AppBar(
+          title: Text(S.current.shopping),
+          actions: [
+            ///설정버튼
+            Button(
+              icon: 'option',
+              type: ButtonType.flat,
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return const SettingBottomSheet();
+                  },
+                );
+              },
+            ),
+          ],
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 8,
+              ),
+              child: Row(
+                children: [
+                  ///검색 입력란
+                  Expanded(
+                    child: InputField(
+                      hint: S.current.searchProduct,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16
+                  ),
+
+                  ///검색 버튼
+                  Button(
+                    onPressed: () {},
+                    icon: 'search',
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
